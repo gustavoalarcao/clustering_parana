64274bed0ba471a11f0cc6040a6a770ea3bbd292
Gustavo Alarcao Ubu <gustavo.alarcao@gmail.com>
2026-05-28 22:05:06 -0300

    Coleta a partir do índice de progressão social

--- config/criar_pasta_de_dados.py
+++ config/criar_pasta_de_dados.py
@@ -2,12 +2,15 @@ from pathlib import Path
 
 
 
+
 if __name__ == '__main__':
     diretorio_raiz = Path(__file__).parent.parent
     
     caminho_brutos = diretorio_raiz / 'dados' / 'brutos'
+    caminho_alterados = diretorio_raiz / 'dados' / 'brutos'
     
     caminho_brutos.mkdir(parents=True, exist_ok=True)
+    caminho_alterados.mkdir(parents=True, exist_ok=True)
     
     print('Pasta "dados/brutos" criada.')
     print('Adicione os arquivos do drive')
